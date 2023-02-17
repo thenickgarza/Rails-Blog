a3214aca966c1e36854f8cfe95db2fbfd2a09845
Nick Garza <thenickgarza14@gmail.com>
2023-02-17 16:02:59 -0600

    fixed the table to display empty space if record does not exist

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,46 +1,52 @@
 class AddressesController < ApplicationController
+  
   before_action :set_address, only: %i[ show edit update destroy ]
+  before_action :set_person
 
-  # GET /addresses or /addresses.json
+  # GET people/:id/addresses or /addresses.json
   def index
-    @person = Person.find(params[:person_id])
     @addresses = @person.addresses.all
   end
 
-  # GET /addresses/1 or /addresses/1.json
+  # GET people/:id/addresses/1 or /addresses/1.json
   def show
     @person = Person.find(params[:person_id])
     @address = @person.addresses.find(params[:id])
   end
 
-  # GET /addresses/new
+  # GET people/:id/addresses/new
   def new
     @person = Person.find(params[:person_id])
     @address = @person.addresses.new
   end
 
-  # GET /addresses/1/edit
+  # GET people/:id/addresses/1/edit
   def edit
   end
 
-  # POST /addresses or /addresses.json
+  # POST people/:id/addresses or /addresses.json
   def create
+    
     @person = Person.find(params[:person_id])
     @address = @person.addresses.new(address_params)
 
     respond_to do |format|
       if @address.save
-        format.html { redirect_to address_url(@address), notice: "Address was successfully created." }
+        format.html { redirect_to person_addresses_path(@address), notice: "Address was successfully created." }
         format.json { render :show, status: :created, location: @address }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @address.errors, status: :unprocessable_entity }
+    
       end
+    
     end
+  
   end
 
-  # PATCH/PUT /addresses/1 or /addresses/1.json
+  # PATCH/PUT people/:id/addresses/1 or /addresses/1.json
   def update
+    
     respond_to do |format|
       if @address.update(address_params)
         format.html { redirect_to address_url(@address), notice: "Address was successfully updated." }
@@ -48,12 +54,16 @@ class AddressesController < ApplicationController
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @address.errors, status: :unprocessable_entity }
+    
       end
+    
     end
+  
   end
 
-  # DELETE /addresses/1 or /addresses/1.json
+  # DELETE people/:id/addresses/1 or /addresses/1.json
   def destroy
+    
     @person = Person.find(params[:person_id])
     @address = @person.addresses.find(params[:id])
     @address.destroy
@@ -61,17 +71,30 @@ class AddressesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to addresses_url, notice: "Address was successfully destroyed." }
       format.json { head :no_content }
+    
     end
+  
   end
 
   private
+    
     # Use callbacks to share common setup or constraints between actions.
     def set_address
+    
       @address = Address.find(params[:id])
+    
+    end
+
+    def set_person
+
+      @person = Person.find(params[:person_id])
+    
     end
 
     # Only allow a list of trusted parameters through.
     def address_params
+    
       params.require(:address).permit(:street, :city, :state, :zipcode)
+    
     end
 end
